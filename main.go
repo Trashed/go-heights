28f8532e2c196483df8f74f8124e688b09f401ca
Jose Kortelahti <jkortelahti@gmail.com>
2017-07-15 22:02:13 +0300

    Added support for generating multiple heightmaps in a row.

--- main.go
+++ main.go
@@ -9,15 +9,18 @@ import (
 var (
 	detail = flag.Int("detail", 6, `Generates height map based on given detail value with following
         formula: 2^d+1. Default detail value is 6 => array size 65.`)
+	numOfMaps = flag.Int("amount", 1, "Number of maps to generate.")
 )
 
 func main() {
 	flag.Parse()
 
 	// Create new Terrain object and generate height map data.
-	t := gen.New(*detail)
-	t.Generate()
+	for i := 0; i < *numOfMaps; i++ {
+		t := gen.New(*detail)
+		t.Generate()
 
-	// Create height map image.
-	gen.Image(*t)
+		// Create height map image.
+		gen.Image(*t)
+	}
 }
